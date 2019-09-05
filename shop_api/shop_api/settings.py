f898abaaeaee6d31da1c5a8677f5ffe754da769b
kelvin <chenzl@akulaku.com>
2019-09-05 10:37:25 +0800

    new version of online shopping

--- shop_api/shop_api/settings.py
+++ shop_api/shop_api/settings.py
@@ -67,7 +67,7 @@ CORS_ALLOW_HEADERS = list(default_headers) + [
 CORS_ORIGIN_WHITELIST = [
 	'http://localhost:8080',
     'http://127.0.0.1:8080',
-    'http://127.0.0.1:80',
+    'http://3.18.144.186:80',
 ]
 
 ROOT_URLCONF = 'shop_api.urls'
