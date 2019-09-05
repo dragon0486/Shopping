ed9ffc23f2078dd935aad1db58345ca9fdac7a03
kelvin <chenzl@akulaku.com>
2019-09-05 10:52:41 +0800

    new version of online shopping

--- shop_api/shop_api/settings.py
+++ shop_api/shop_api/settings.py
@@ -68,7 +68,7 @@ CORS_ORIGIN_WHITELIST = [
 	'http://localhost:8080',
     'http://127.0.0.1:8080',
     'http://3.18.144.186:80',
-    'http://3.18.144.186',
+    'http://3.18.144.186:8080',
 ]
 
 ROOT_URLCONF = 'shop_api.urls'
